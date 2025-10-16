fb3aaebe9af82d221d267af579776683c3f6fa27
Lucien Lopez <luk.f.92@gmail.com>
2025-10-16 21:03:36 +0200

    Order importer

---
+++ app/services/orders_importer.rb
@@ -0,0 +1,34 @@
+class OrdersImporter < ApplicationService
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def call
+    require 'csv'
+
+    csv_data = File.readlines(@file_path, encoding: 'UTF-8')[4..] # Skip first 4 lines
+
+    CSV.parse(csv_data.join, headers: true, col_sep: ';') do |row|
+      next unless row['Type d’opération'] == 'Achat'
+      next unless row['Statut'] == 'Exécuté'
+
+      unless stock = Stock.find_by(isin: row['Isin'])
+        stock = Stock.create!(
+          isin: row['Isin'],
+          name: row['Nom']
+        )
+      end
+
+      investment = Investment.find_or_initialize_by(
+        stock: stock,
+        reference_number: row['Référence'],
+      )
+      investment.update!(
+        shares: row['Quantité'].to_i,
+        purchase_price: row['Cours d’exécution'].to_s.gsub(/[^\d.,]/, '').tr(',', '.').to_f,
+        total_price: row['Montant'].to_s.gsub(/[^\d.,]/, '').tr(',', '.').to_f.abs,
+        purchased_at: Date.parse(row['Date de création']),
+      )
+    end
+  end
+end
