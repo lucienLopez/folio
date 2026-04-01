30d31ab5c17e6b3fa414deb7266a464e9cbc23b9
Lucien Lopez <luk.f.92@gmail.com>
2026-04-01 14:29:18 +0200

    Positions: gradient coloring

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 module PositionsHelper
-  def position_row_class(position)
+  def position_row_style(position)
     return unless position.gain_pct
 
-    case position.gain_pct
-    when (30..)     then 'row-gain-strong'
-    when (5...30)   then 'row-gain-mild'
-    when (-5...5)   then nil
-    when (-20...-5) then 'row-loss-mild'
-    else                 'row-loss-strong'
-    end
+    gain = position.gain_pct.to_f
+    intensity = [(gain.abs / 30.0), 1.0].min * 0.4
+    return if intensity < 0.01
+
+    color = gain >= 0 ? "34 197 94" : "239 68 68"
+    "--row-color: #{color}; --row-alpha: #{intensity.round(3)};"
   end
 end
